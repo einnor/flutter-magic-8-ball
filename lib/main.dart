8b66a1bdc155825e4ff6f73c4f95f2dfae80d4b2
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-15 15:00:06 +0300

    Added stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,20 @@ void main() {
           title: Text('Ask Me Anything'),
           backgroundColor: Colors.blue.shade900,
         ),
+        body: App(),
       ),
     ),
   );
+}
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
